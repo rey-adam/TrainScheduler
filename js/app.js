4cf31d5df3294c5726d40030547d2dbc943fe61e
Rey Cruz <reycruz@Reys-MacBook-Pro.local>
2017-11-18 22:50:53 -0800

    HTML done, firebase database included in js, css styling added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 // alert("hello") connected 
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC3snNoC54EStD2ftbTHChLMwtJuBW3ZKk",
+    authDomain: "train-scheduler-44be9.firebaseapp.com",
+    databaseURL: "https://train-scheduler-44be9.firebaseio.com",
+    projectId: "train-scheduler-44be9",
+    storageBucket: "train-scheduler-44be9.appspot.com",
+    messagingSenderId: "1069884267137"
+  };
+  firebase.initializeApp(config);
+
+  
